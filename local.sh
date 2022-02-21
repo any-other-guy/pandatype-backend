649dab79ea11c4e06aa1267eec6822e64dceb6c1
ChunKyYoinky <ckyue@my.yorku.ca>
2022-02-21 18:36:12 -0500

    springboot version to 2.6.3

--- local.sh
+++ local.sh
@@ -1,8 +1,10 @@
+# build jar files for services
 cd auth; ./mvnw package -DskipTests; cd ..;
 cd typingtest; ./mvnw package -DskipTests; cd ..;
 cd leaderboard; ./mvnw package -DskipTests; cd ..;
+
+# cleanup and restart
 docker-compose down;
-#docker rm -f $(docker ps -a -q);
 docker rmi -f $(docker images -a | grep -v "mysql" | awk 'NR>1 {print $3}')
 docker volume rm $(docker volume ls -q);
 docker-compose up;
--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
+# build jar files for services
 cd auth; mvn package -DskipTests; cd ..;
 cd typingtest; mvn package -DskipTests; cd ..;
 cd leaderboard; mvn package -DskipTests; cd ..;
 
+# generate private/public keys for auth service
 cd auth/src/main/resources;
 openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048;
 openssl rsa -pubout -in private.pem -out public_key.pem;
@@ -9,7 +11,7 @@ openssl pkcs8 -topk8 -in private.pem -nocrypt -out private_key.pem
 cp public_key.pem ../../../../leaderboard/src/main/resources;
 cd ../../../../;
 
+# cleanup and restart
 docker-compose down;
-#docker rm -f $(docker ps -a -q);
 docker rmi -f $(docker images -a | grep -v "mysql" | awk 'NR>1 {print $3}')
 docker-compose up;
